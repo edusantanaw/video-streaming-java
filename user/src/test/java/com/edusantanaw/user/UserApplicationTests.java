e8311318b97b8beb77a2e6772589de258643fdfd
edusantanaw <eduardosantana20122002@gmail.com>
2025-04-06 19:32:19 -0300

    chore: initial configs

---
+++ user/src/test/java/com/edusantanaw/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusantanaw.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
